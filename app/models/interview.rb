54ce38b65051827b800fbdb465395f733f1aed1a
Yukito Ito <yukibukiyou@gmail.com>
2018-04-11 18:37:01 +0900

    allow_nil: true

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,5 +2,5 @@ class Interview < ApplicationRecord
   belongs_to :person
   belongs_to :staff, optional: true
   delegate :name, to: :person, prefix: true
-  delegate :name, to: :staff, prefix: true
+  delegate :name, to: :staff, prefix: true, allow_nil: true
 end
