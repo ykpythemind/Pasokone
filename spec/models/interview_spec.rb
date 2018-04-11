bd778df6f7cf24169660c789f95bda9cc22da5a5
Yukito Ito <yukibukiyou@gmail.com>
2018-04-11 21:57:12 +0900

    fix - contextの文言 - ブロック前後のスペース

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
   describe '#staff_name' do
+
     subject { interview.staff_name }
     let (:person) { create(:person) }
     let (:staff) { create(:staff) }
-    context 'staff exists' do
+
+    context 'when staff exists' do
       let (:interview) { Interview.create(person: person, staff: staff) }
       it { is_expected.to eq staff.name }
     end
-    context 'staff does not exist' do
+
+    context 'when staff does not exist' do
       let (:interview) { Interview.create(person: person) }
       it { is_expected.to be_nil }
     end
